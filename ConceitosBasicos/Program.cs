82deb50c12c585103fb41dd8b0f72fc16e8d531e
Arthur Gama <114451612+IamArthurGama@users.noreply.github.com>
2025-08-17 15:07:47 -0300

    123

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -5,3 +5,9 @@ nome = Console.ReadLine();
 Console.Write("Digite sua idade: ");
 idade = Convert.ToByte(Console.ReadLine());
 Console.WriteLine("Olá " + nome + ". A idade informada foi {0}", idade);
+Console.WriteLine($"A nova idade é {idade++}.");    
+Console.WriteLine($"A nova idade é {++idade}.");    
+
+//altere o programa para que solicite ao usuário informar o seu salario e ao final informe o desconto
+//de IRPF de acordo com o salario informado. Utilize a tabela oficial de descontos do IRPF do Basil.
+
